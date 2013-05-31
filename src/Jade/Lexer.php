6c4e1bca2d239e5f2183b0ac646a552b76ed6f9d
Sébastien ELET <sebastien@elet.fr>
2013-05-31 23:52:37 +0200

    php-cs-fix

--- src/Jade/Lexer.php
+++ src/Jade/Lexer.php
@@ -2,8 +2,8 @@
 
 namespace Jade;
 
-class Lexer {
-
+class Lexer
+{
     protected $deferredObjects   = array();
     protected $page;
     private $last;
@@ -16,10 +16,11 @@ class Lexer {
     /**
      * Set lexer input.
      *
-     * @param   string  $input  input string
+     * @param string $input input string
      */
-    public function setInput($input) {
-		$this->adjustLineDelimiter($input);
+    public function setInput($input)
+    {
+        $this->adjustLineDelimiter($input);
         $this->page            = preg_replace(array('/\r\n|\r/', '/\t/'), array("\n", '  '), $input); // TODO: Tab
         $this->deferredObjects  = array();
         $this->lastIndents      = 0;
@@ -27,15 +28,14 @@ class Lexer {
         $this->stash            = array();
     }
 
-
-
     /**
      * Return next token or previously stashed one.
      *
-     * @return  Object
+     * @return Object
      */
-    public function getAdvancedToken() {
-        if ( $token = $this->getStashedToken() ) {
+    public function getAdvancedToken()
+    {
+        if ($token = $this->getStashedToken() ) {
             return $token;
         }
 
@@ -45,23 +45,25 @@ class Lexer {
     /**
      * Defer token.
      *
-     * @param   Object   $token  token to defer
+     * @param Object $token token to defer
      */
-    public function deferToken(\stdClass $token) {
+    public function deferToken(\stdClass $token)
+    {
         $this->deferredObjects[] = $token;
     }
 
     /**
      * Predict for number of tokens.
      *
-     * @param   integer     $number number of tokens to predict
+     * @param integer $number number of tokens to predict
      *
-     * @return  Object              predicted token
+     * @return Object predicted token
      */
-    public function predictToken($number = 1) {
+    public function predictToken($number = 1)
+    {
         $fetch = $number - count($this->stash);
 
-        while ( $fetch-- > 0 ) {
+        while ($fetch-- > 0) {
             $this->stash[] = $this->getNextToken();
         }
 
@@ -71,12 +73,13 @@ class Lexer {
     /**
      * Construct token with specified parameters.
      *
-     * @param   string  $type   token type
-     * @param   string  $value  token value
+     * @param string $type  token type
+     * @param string $value token value
      *
-     * @return  Object          new token object
+     * @return Object new token object
      */
-    public function takeToken($type, $value = null) {
+    public function takeToken($type, $value = null)
+    {
         return (Object) array(
             'type'  => $type
           , 'line'  => $this->lineno
@@ -87,35 +90,38 @@ class Lexer {
     /**
      * Return stashed token.
      *
-     * @return  Object|boolean   token if has stashed, false otherways
+     * @return Object|boolean token if has stashed, false otherways
      */
-    protected function getStashedToken() {
+    protected function getStashedToken()
+    {
         return count($this->stash) ? array_shift($this->stash) : null;
     }
 
     /**
      * Return deferred token.
      *
-     * @return  Object|boolean   token if has deferred, false otherways
+     * @return Object|boolean token if has deferred, false otherways
      */
-    protected function getDeferredToken() {
+    protected function getDeferredToken()
+    {
         return count($this->deferredObjects) ? array_shift($this->deferredObjects) : null;
     }
 
     /**
      * Return next token.
      *
-     * @return  Object
+     * @return Object
      */
-    protected function getNextToken() {
+    protected function getNextToken()
+    {
         $scanners = array(
             'INCLUDE'
           , 'getDeferredToken'
           , 'scanEOS'
+          , 'DOCUMENT_TYPE'
           , 'TAG'
           , 'FILTER'
           , 'scanCode'
-          , 'DOCUMENT_TYPE'
           , 'ID'
           , 'CLASS'
           , 'scanAttributes'
@@ -125,34 +131,36 @@ class Lexer {
           , 'TEXT'
         );
 
-        foreach ( $scanners as $scan ) {
-			if (preg_match('/^scan|get/', $scan)) {
-				$token = $this->$scan();
-			} else {
-				$token = $this->next($scan);
-			}
+        foreach ($scanners as $scan) {
+            if (preg_match('/^scan|get/', $scan)) {
+                $token = $this->$scan();
+            } else {
+                $token = $this->next($scan);
+            }
 
-            if ( $token ) {
+            if ($token) {
                 return $token;
             }
         }
     }
-		
-    protected function scanDotBlock() {
-			if ( $this->page[0] === '.' ) {
-				$token = $this->takeToken('text', $this->next('dotBlock')->value);
-				return $token;
-			}
-			else return null;
+
+    protected function scanDotBlock()
+    {
+            if ($this->page[0] === '.') {
+                $token = $this->takeToken('text', $this->next('dotBlock')->value);
+
+                return $token;
+            } else return null;
     }
 
     /**
      * Scan EOS from input & return it if found.
      *
-     * @return  Object|null
+     * @return Object|null
      */
-    protected function scanEOS() {
-        if ( mb_strlen($this->page) ) {
+    protected function scanEOS()
+    {
+        if (mb_strlen($this->page) ) {
             return;
         }
 
@@ -162,12 +170,13 @@ class Lexer {
     /**
      * Scan comment from input & return it if found.
      *
-     * @return  Object|null
+     * @return Object|null
      */
-    protected function scanComment() {
+    protected function scanComment()
+    {
         $matches = array();
 
-        if ( preg_match('/^ *\/\/(-)?([^\n]+)?/', $this->page, $matches) ) {
+        if (preg_match('/^ *\/\/(-)?([^\n]+)?/', $this->page, $matches) ) {
             $this->reduce($matches[0]);
             $token = $this->takeToken('comment', isset($matches[2]) ? $matches[2] : '');
             $token->buffer = !isset($matches[1]) || '-' !== $matches[1];
@@ -176,16 +185,17 @@ class Lexer {
         }
     }
 
-    protected function scanCode() {
+    protected function scanCode()
+    {
         $matches = array();
 
-        if ( preg_match('/^(!?=|-)([^\n]+)/', $this->page, $matches) ) {
+        if (preg_match('/^(!?=|-)([^\n]+)/', $this->page, $matches) ) {
             $this->reduce($matches[0]);
 
             $flags = $matches[1];
             $token = $this->takeToken('code', $matches[2]);
             $token->buffer = (isset($flags[0]) && '=' === $flags[0]) || (isset($flags[1]) && '=' === $flags[1]);
-			$token->code_type = $matches[1];
+            $token->code_type = $matches[1];
 
             return $token;
         }
@@ -194,65 +204,67 @@ class Lexer {
     /**
      * Scan attributes from input & return them if found.
      *
-     * @return  Object|null
+     * @return Object|null
      */
-    protected function scanAttributes() {
-        if ( $this->page[0] === '(' ) {
+    protected function scanAttributes()
+    {
+        if ($this->page[0] === '(' ) {
             $token = $this->takeToken('attributes', '');
             $token->attributes = array();
-			//pass '('
-			$this->next('ATTRIBUTE');
+            //pass '('
+            $this->next('ATTRIBUTE');
 
-			do {
-				$attr = $this->next('ATTRIBUTE');
-				if ($attr[0] == '') {
-					//TODO Error reporting
-					return null;
-				}
-				$attr = array_values(preg_grep('/^.+$/', $attr));
+            do {
+                $attr = $this->next('ATTRIBUTE');
+                if ($attr[0] == '') {
+                    //TODO Error reporting
+                    return null;
+                }
+                $attr = array_values(preg_grep('/^.+$/', $attr));
 
-				if (isset($attr[1]) && isset($attr[2])) {
-					$token->attributes[$attr[1]] = $attr[2];
-				} elseif (isset($attr[1]) && $attr[1] != ',' && $attr[1] != ')') {
-					$token->attributes[$attr[1]] = '"'.$attr[1].'"';
-				}
-			} while ($attr[1] != ')');
+                if (isset($attr[1]) && isset($attr[2])) {
+                    $token->attributes[$attr[1]] = $attr[2];
+                } elseif (isset($attr[1]) && $attr[1] != ',' && $attr[1] != ')') {
+                    $token->attributes[$attr[1]] = '"'.$attr[1].'"';
+                }
+            } while ($attr[1] != ')');
+
             return $token;
         }
     }
 
-    protected function scanIndentation() {
+    protected function scanIndentation()
+    {
         $matches = array();
 
-        if ( preg_match('/^\n( *)/', $this->page, $matches) ) {
+        if (preg_match('/^\n( *)/', $this->page, $matches)) {
             $this->lineno++;
             $this->reduce($matches[0]);
 
             $token      = $this->takeToken('indent', $matches[1]);
             $indents    = mb_strlen($token->value) / 2;
 
-
-            if ( mb_strlen($this->page) && $this->page[0] === "\n" ) {
+            if (mb_strlen($this->page) && $this->page[0] === "\n") {
                 $token->type = 'newline';
 
                 return $token;
-            } elseif ( $indents % 1 !== 0 ) {
+            } elseif ($indents % 1 !== 0) {
                 throw new \Exception(sprintf(
                     'Invalid indentation found. Spaces count must be a multiple of two, but %d got.'
                   , mb_strlen($token->value)
                 ));
-            } elseif ( $indents === $this->lastIndents ) {
+            } elseif ($indents === $this->lastIndents) {
                 $token->type = 'newline';
-            } elseif ( $indents > $this->lastIndents + 1 ) {
+            } elseif ($indents > $this->lastIndents + 1) {
                 throw new \Exception(sprintf(
                     'Invalid indentation found. Got %d, but expected %d.'
                   , $indents
                   , $this->lastIndents + 1
                 ));
-            } elseif ( $indents < $this->lastIndents ) {
+            } elseif ($indents < $this->lastIndents) {
                 $count = $this->lastIndents - $indents;
                 $token->type = 'outdent';
-                while ( --$count ) {
+                while (--$count) {
                     $this->deferToken($this->takeToken('outdent'));
                 }
             }
@@ -263,16 +275,20 @@ class Lexer {
         }
     }
 
-    function reduce($bytes) {
+    public function reduce($bytes)
+    {
         $this->page = mb_substr($this->page, mb_strlen($bytes));
     }
-    function length() {
+    public function length()
+    {
         return mb_strlen($this->page);
     }
-    function adjustLineDelimiter($bytes) {
+    public function adjustLineDelimiter($bytes)
+    {
         $this->page = preg_replace('/\r\n|\r/', '\n', $bytes);
     }
-    function next($type) {
+    public function next($type)
+    {
         //echo $type.PHP_EOL;
         $map = array(
             'INCLUDE'=>'/^include +([^\n]+)/',
@@ -295,11 +311,14 @@ class Lexer {
         );
 
         if (preg_match($map[$type], $this->page, $parentheses)) {
+
             $this->reduce($parentheses[0]);
+
             return $this->item($type, $parentheses);
         }
     }
-    function item($type, $data) {
+    public function item($type, $data)
+    {
         //echo sprintf('%-16s  %s'.PHP_EOL, $type, trim($data[0]));
         $remap= array(
             'DOCUMENT_TYPE'=>'doctype',
@@ -326,10 +345,7 @@ class Lexer {
         if ($type == 'ATTRIBUTE') {
             return $data;
         }
+
         return (object) array('type'=>$remap[$type], 'value'=>$data[$index]);
     }
 }
-
-
-
-?>
