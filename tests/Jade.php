e116d685a7c2f42ba5f4f94dc029171bb9fef173
Sébastien ELET <sebastien@elet.fr>
2013-05-31 23:05:18 +0200

    Add new tests

--- tests/Jade.php
+++ tests/Jade.php
@@ -45,4 +45,23 @@ class Jade extends atoum
             ->isEqualTo($this->parse(implode("\r", $tags)))
             ->isEqualTo($this->parse(implode("\n", $tags)));
     }
+
+    public function testSingleQuotes()
+    {
+        $this->string("<p>'foo'</p>")
+            ->isEqualTo($this->parse("p 'foo'"));
+        $this->string("<p>\n  'foo'\n</p>")
+            ->isEqualTo($this->parse("p\n  | 'foo'"));
+    }
+
+    public function testTags()
+    {
+        $str = implode("\n", array('p', 'div', 'img'));
+        $html = implode("\n", array('<p></p>', '<div></div>', '<img />'));
+
+        $this->string($html)
+            ->isEqualTo($this->parse($str));
+        $this->string('<div id="item" class="something"></div>')
+            ->isEqualTo($this->parse('div#item.something'));
+    }
 }
