9e07956e6b28bd139e94c66e27f4f33499f5c93a
Sébastien ELET <sebastien@elet.fr>
2013-06-01 03:07:51 +0200

    Add tests (most of them fails and are comment...)

--- tests/Jade.php
+++ tests/Jade.php
@@ -85,9 +85,185 @@ class Jade extends atoum
             ->isEqualTo($this->parse('div#item.something'));
         $this->string('<div id="foo" class="bar"></div>')
             ->isEqualTo($this->parse('#foo.bar'))
-        //    ->isEqualTo($this->parse('.bar#foo'))
+            //    ->isEqualTo($this->parse('.bar#foo'))
             ->isEqualTo($this->parse('div#foo(class="bar")'))
             ->isEqualTo($this->parse('div(class="bar")#foo'));
         //    ->isEqualTo($this->parse('div(id="bar").foo'))
+        $this->string('<div class="foo bar baz"></div>')
+            ->isEqualTo($this->parse('div.foo.bar.baz'));
+        //    ->isEqualTo($this->parse('div(class="foo").bar.baz'))
+        //    ->isEqualTo($this->parse('div.foo(class="bar").baz'))
+        //    ->isEqualTo($this->parse('div.foor.bar(class="baz"'))
+        $this->string('<div class="a-b2"></div>')
+            ->isEqualTo($this->parse('div.a-b2'));
+
+        $this->string('<div class="a_b2"></div>')
+            ->isEqualTo($this->parse('div.a_b2'));
+        $this->string('<fb:user></fb:user>')
+            ->isEqualTo($this->parse('fb:user'));
+        $this->string('<fb:user:role></fb:user:role>')
+            ->isEqualTo($this->parse('fb:user:role'));
+        // $this->string('<colgroup><col class="test"/></colgroup>')
+        //     ->isEqualTo($this->parse("colgroup\n  col.test"));
+    }
+
+    public function testTabConversion()
+    {
+        $str = implode(
+            "\n",
+            array(
+                'ul',
+                "\tli a",
+                "\t",
+                "\tli b",
+                "\t\t",
+                "\t\t\t\t\t\t",
+                "\tli",
+                "\tli",
+                "\t\tul",
+                "\t\t\tli c",
+                "",
+                "\t\t\tli d",
+                "\tli e"
+            )
+        );
+
+        $html = implode(
+            '',
+            array(
+                '<ul>',
+                '<li>a</li>',
+                '<li>b</li>',
+                '<li><ul><li>c</li><li>d</li></ul></li>',
+                '<li>e</li>',
+                '</ul>'
+            )
+        );
+
+        //$this->string($html)
+        //    ->isEqualTo($this->parse($str));
+    }
+
+    public function testNewLines()
+    {
+        $str = implode(
+            "\n",
+            array(
+                'ul',
+                '  li a',
+                '  ',
+                '    ',
+                '',
+                ' ',
+                '  li b',
+                '  li',
+                '    ',
+                '        ',
+                ' ',
+                '    ul',
+                '      ',
+                '      li c',
+                '      li d',
+                '  li e'
+            )
+        );
+
+        $html = implode(
+            '',
+            array(
+                '<ul>',
+                '<li>a</li>',
+                '<li>b</li>',
+                '<li><ul><li>c</li><li>d</li></ul></li>',
+                '<li>e</li>',
+                '</ul>'
+            )
+        );
+
+        //$this->string($html)
+        //    ->isEqualTo($this->parse($str));
+
+        $str = implode(
+            "\n",
+            array(
+                'html',
+                ' ',
+                '  head',
+                '    != "test"',
+                '  ',
+                '  ',
+                '  ',
+                '  body'
+            )
+        );
+
+        $html = implode(
+            '',
+            array(
+                '<html>',
+                '<head>',
+                'test',
+                '</head>',
+                '<body></body>',
+                '</html>'
+            )
+        );
+
+        //$this->string($html)
+        //    ->isEqualTo($this->parse($str));
+
+        //$this->string('<foo></foo>something<bar></bar>')
+        //    ->isEqualTo($this->parse('foo\n= "something"\nbar'));
+        //$this->string('<foo></foo>something<bar></bar>else')
+        //    ->isEqualTo($this->parse('foo\n= "something"\nbar\n= "else"'));
+
+    }
+
+    public function testSupportText()
+    {
+        //$this->string('foo\nbar\nbaz')
+        //    ->isEqualTo($this->parse('| foo\n| bar\n| baz'));
+        //$this->string('foo \nbar \nbaz')
+        //    ->isEqualTo($this->parse('| foo \n| bar \n| baz'));
+        $this->string('(hey)')
+            ->isEqualTo($this->parse('| (hey)'));
+        $this->string('some random text')
+            ->isEqualTo($this->parse('| some random text'));
+        $this->string('  foo')
+            ->isEqualTo($this->parse('|   foo'));
+        $this->string('  foo  ')
+            ->isEqualTo($this->parse('|   foo  '));
+        //$this->string('  foo  \n bar    ')
+        //    ->isEqualTo($this->parse('|   foo  \n|  bar    '));
+    }
+
+    public function testPipeLessText()
+    {
+        //$this->string('<pre><code><foo></foo><bar></bar></code></pre>')
+        //    ->isEqualTo($this->parse('pre\n  code\n    foo\n\n    bar'));
+        //$this->string('<p>foo\n\nbar</p>')
+        //    ->isEqualTo($this->parse('p.\n  foo\n\n  bar'));
+        //$this->string('<p>foo\n\n\n\nbar</p>')
+        //    ->isEqualTo($this->parse('p.\n  foo\n\n\n\n  bar'));
+        //$this->string('<p>foo\n  bar\nfoo</p>')
+        //    ->isEqualTo($this->parse('p.\n  foo\n    bar\n  foo'));
+        //$this->string('<script>s.parentNode.insertBefore(g,s)</script>')
+        //    ->isEqualTo($this->parse('script.\n  s.parentNode.insertBefore(g,s)\n'));
+        //$this->string('<script>s.parentNode.insertBefore(g,s)</script>')
+        //    ->isEqualTo($this->parse('script.\n  s.parentNode.insertBefore(g,s)'));
+    }
+
+    public function testTagText()
+    {
+        $this->string('<p>some random text</p>')
+            ->isEqualTo($this->parse('p some random text'));
+        //$this->string('<p>click<a>Google</a>.</p>')
+        //    ->isEqualTo($this->parse('p\n  | click\n  a Google\n  | .'));
+        $this->string('<p>(parens)</p>')
+            ->isEqualTo($this->parse('p (parens)'));
+        $this->string('<p foo="bar">(parens)</p>')
+            ->isEqualTo($this->parse('p(foo="bar") (parens)'));
+        // $this->string('<option value="">-- (optional) foo --</option>')
+        //    ->isEqualTo($this->parse('option(value="") -- (optional) foo --'));
     }
 }
